e6179b5cb102136c23cca6e341554c05b0eaf35f
Gustavo Javier Aguirre <gjagui@gmail.com>
2023-01-17 17:48:06 -0300

    Added messages and user information endpoints

---
+++ backend/controller/message.controller.ts
@@ -0,0 +1,55 @@
+import { Request, Response } from "express";
+import { getManager } from "typeorm";
+
+import { Message } from "../src/entity/message.entity";
+import { User } from "../src/entity/user.entity";
+import { Subscription } from "../src/entity/subscription.entity";
+import { Notification } from "../src/entity/notification.entity";
+
+export const Messages = async (req: Request, res: Response) => {
+    try {
+        const messageRepository = getManager().getRepository(Message);
+
+        const messages = await messageRepository.find({
+            where: {
+                user: {
+                    id: Number(req.params.id)
+                },
+            },
+            relations: ["user", "subscription", "notification"]
+        });
+
+        res.send(messages);
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).send("Internal Server Error");
+    }
+}
+
+export const SendMessages = async (req: Request, res: Response) => {
+    try {
+        const messageRepository = getManager().getRepository(Message);
+
+        let message = new Message();
+
+        const user = new User();
+        user.id = req.body.user_id;
+
+        const subscription = new Subscription();
+        subscription.id = req.body.subscription_id;
+
+        const notification = new Notification();
+        notification.id = req.body.notification_id;
+
+        message = { ...message, user, subscription, notification };
+
+        const savedMessage = await messageRepository.save(message);
+
+        res.send(savedMessage);
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).send("Internal Server Error");
+    }
+}
--- backend/src/router/router.ts
+++ backend/src/router/router.ts
@@ -1,7 +1,12 @@
 import express, { Router } from "express";
 
 import { Users } from "../../controller/user.controller";
+import { Messages, SendMessages } from "../../controller/message.controller";
 
 export const routers = (router: Router) => {
+
     router.get("/users", Users);
+
+    router.get("/user/:id/message", Messages);
+    router.post("/message", SendMessages);
 };
